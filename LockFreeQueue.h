bc3c43a0a6968c930362606a0ba76e90f7ecaa20
HaronK <okreptul@yahoo.com>
2015-07-25 02:59:31 +0200

    Add proper author name in the license

--- LockFreeQueue.h
+++ LockFreeQueue.h
@@ -1,7 +1,7 @@
 /**
  * The MIT License (MIT)
  *
- * Copyright (c) 2015 HaronK
+ * Copyright (c) 2015 Oleg Khryptul aka HaronK
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
